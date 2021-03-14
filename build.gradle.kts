363424d37e87a505e9ee69e93806b15ac952bcb4
vjuge <vincent.juge@gmail.com>
2021-03-14 23:23:07 +0100

    added publication

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     kotlin("jvm") version "1.3.71"
     idea
     `maven-publish`
+    id("org.jetbrains.dokka") version "1.4.20"
+    signing
 }
 
 val cdm_version = "2.85.0"
@@ -15,6 +17,7 @@ version = "${cdm_version}"
 
 repositories {
     mavenCentral()
+    jcenter()
     maven {
         // Restrict to CDM dependencies
         content {
@@ -23,8 +26,15 @@ repositories {
         }
         setUrl("https://regnosys.jfrog.io/artifactory/libs-snapshot")
         credentials {
-            username = "isda"
-            password = "isda"
+            username = project.properties["isdaLogin"] as String?
+            password = project.properties["isdaPwd"] as String?
+        }
+    }
+    maven {
+        setUrl("https://s01.oss.sonatype.org")
+        credentials {
+            username = project.properties["iossrhUsername"] as String?
+            password = project.properties["ossrhPassword"] as String?
         }
     }
 }
@@ -44,14 +54,66 @@ tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+repositories {
+    maven {
+        setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+        credentials {
+            username = project.properties["iossrhUsername"] as String?
+            password = project.properties["ossrhPassword"] as String?
+        }
+    }
+}
+
+
 publishing {
     publications {
-        create<MavenPublication>("maven") {
+        create<MavenPublication>("mavenPublish") {
             groupId = "${group}"
             artifactId = "${artifactId}"
             version = "${version}"
 
             from(components["java"])
+            pom {
+                name.set("cdmdsl")
+                description.set("a kotlin dsl for isda cdm")
+                packaging = "jar"
+                url.set("https://github.com/vjuge/cdmdsl")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("vjuge")
+                        name.set("Vincent Jugé")
+                        email.set("vincent.juge@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/vjuge/cdmdsl")
+                    url.set("https://github.com/vjuge/cdmdsl")
+                }
+            }
+        }
+        repositories {
+            maven {
+                setUrl("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+                credentials {
+                    username = project.properties["iossrhUsername"] as String?
+                    password = project.properties["ossrhPassword"] as String?
+                }
+            }
         }
     }
+}
+
+signing {
+    sign(publishing.publications["mavenPublish"])
 }
