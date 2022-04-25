7432ce6a8bbaacc29eb70d458b9361a24f7cebee
vincent <vincent.juge@gmail.com>
2022-04-25 09:58:07 +0200

    feat(upgrade): upgrade_v2.161.0 (#10)

--- versions.gradle.kts
+++ versions.gradle.kts
@@ -1,10 +1,10 @@
 mapOf(
     "appCompatVersion" to "1.1.0",
-    "cdm_version" to "2.143.2",
+    "cdm_version" to "2.161.0",
     "group" to "com.github.vjuge",
     "artifactId" to "cdmdsl",
     "classGraph" to "4.8.116",
-    "patch_version" to "1",
+    "patch_version" to "0",
     "junit_version" to "5.6.0",
     "kotlinpoet_version" to "1.6.0"
 ).forEach { (name : String, version: String) ->
